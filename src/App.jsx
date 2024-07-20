a29cf1e0ee2069191a6a9718cf8a6a724e5940bb
Goutam-04 <goutamnayak2804+github1@gmail.com>
2024-07-20 22:13:07 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="overflow-hidden bg-contain bg-center h-screen w-screen bg-[url('/background.png')]">
           <div className="absolute top-[-20vw] right-[5vw] w-[30vw] h-[30vw] bg-fade-circle backdrop-blur-lg rounded-full"></div>
-          <div className="absolute top-[-20vw] right-[5vw] w-[30vw] h-[30vw] bg-fade-circle backdrop-blur-lg rounded-full"></div>
+
+          <div className="absolute top-[-20vw] right-[15vw] w-[30vw] h-[30vw] bg-fade-circle backdrop-blur-lg rounded-full"></div>
+
     <Navbar/>
     <Home/>
    </div>
